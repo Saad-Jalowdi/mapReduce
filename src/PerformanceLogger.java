6c25945ae714fe5e0b678db6720aba6fb007bccb
saad <saad.tarek.jal@gmail.com>
2020-01-19 02:17:10 +0200

    PerformanceLogger now prints memory

--- src/PerformanceLogger.java
+++ src/PerformanceLogger.java
@@ -5,34 +5,40 @@ import java.math.BigInteger;
 
 public class PerformanceLogger {
 
-    private String loggerName ;
-    private BigInteger startingTime; //in ms;
-    private BigInteger endingTime; //in ms;
+    private String loggerName;
+    private BigInteger startingTime; //in ms
+    private BigInteger endingTime; //in ms
+    private BigInteger startingMem;//in bytes
+    private BigInteger endingMem;//in bytes
 
-    private PerformanceLogger(String loggerName){
+    private PerformanceLogger(String loggerName) {
         this.loggerName = loggerName;
     }
 
-    public static PerformanceLogger getLogger(String name){
+    public static PerformanceLogger getLogger(String name) {
         return new PerformanceLogger(name);
     }
 
-    public void start(){
+    public void start() {
         startingTime = BigInteger.valueOf(System.currentTimeMillis());
+        startingMem = BigInteger.valueOf(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
     }
 
-    public void stop(){
+    public void stop() {
         endingTime = BigInteger.valueOf(System.currentTimeMillis());
+        endingMem = BigInteger.valueOf(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
     }
 
     public void log() {
         try {
-            File file = new File("/map_reduce/performance_for_"+loggerName+".txt");
-            if (!file.exists()){
+            File file = new File("/map_reduce/performance_for_" + loggerName + ".txt");
+            if (!file.exists()) {
                 file.createNewFile();
             }
-            FileWriter fileWriter = new FileWriter("/map_reduce/performance_for_"+loggerName+".txt");
-            fileWriter.write(endingTime.subtract(startingTime).toString()+"ms");
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write(loggerName + " execution time : " + endingTime.subtract(startingTime).toString() + " milliseconds\n");
+            fileWriter.write(loggerName + " used memory : " + endingMem.subtract(startingMem).toString() + " bytes\n");
+
             fileWriter.flush();
             fileWriter.close();
         } catch (IOException e) {
